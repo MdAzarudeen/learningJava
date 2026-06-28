25ac5b01834e0825739a04b35cfb0c5291011985
MdAzarudeen <mohazar671@gmail.com>
2026-06-28 11:14:23 +0530

    secondCommit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, This is to understand git commands");
+        System.out.println("this is Azar");
     }
 }
